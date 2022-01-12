a892ea4e0b12a34eb8afa02ffda95f3bc30567fa
Michelangelo Bartolomucci <3181748+mkexc@users.noreply.github.com>
2022-01-12 12:48:34 +0100

    feat: Updated Controller testbench

--- bist/CONTROLLER/tb_controller.vhd
+++ bist/CONTROLLER/tb_controller.vhd
@@ -14,16 +14,26 @@ architecture rtl of tb_controller is
         port (
             clk, rst, TEST : in std_logic;
             MISR_OUT: in std_logic_vector(N_MISR-1 downto 0);
-            GO, TPG_ODE_MUX_en: out std_logic
+            SEED_ADDR: out std_logic_vector(3 downto 0);
+            GO, TPG_MUX_en, ODE_en, END_TEST, TPG_LD, SCAN_EN, DUT_RESET, LFSR_MISR_RESET: out std_logic
         );
     end component controller;
 
-    signal clk_s,rst_s,TEST_s,GO_s,TPG_ODE_MUX_en_s : std_logic;
+    component ROM is
+        port (
+            addr: in std_logic_vector(3 downto 0);
+            dout: out std_logic_vector(N_LFSR-1 downto 0)
+        );
+    end component ROM;
+
+    signal clk_s,rst_s,TEST_s,GO_s,TPG_MUX_en_s, ODE_en_s, END_TEST_s, TPG_LD_s, SCAN_EN_s, DUT_RESET_s, LFSR_MISR_RESET_s : std_logic;
     signal MISR_OUT_s : std_logic_vector(N_MISR-1 downto 0);
+    signal ROM_OUT_s : std_logic_vector(N_LFSR-1 downto 0);
+    signal SEED_ADDR_s : std_logic_vector(3 downto 0);
     constant clkper : time := 20 ns;
 begin
     
-    uut: controller 
+    ctlr: controller 
     generic map (
         GOLDEN_SIGNATURE => "0101010100101010101001010101001010101010010101010010011010101010"
     )
@@ -32,10 +42,23 @@ begin
         rst=>rst_s,
         TEST=>TEST_s,
         MISR_OUT=>MISR_OUT_s,
+        SEED_ADDR=>SEED_ADDR_s,
         GO=>GO_s,
-        TPG_ODE_MUX_en=>TPG_ODE_MUX_en_s
+        TPG_MUX_en=>TPG_MUX_en_s,
+        ODE_en=>ODE_en_s,
+        END_TEST=>END_TEST_s,
+        TPG_LD=>TPG_LD_s,
+        SCAN_EN=>SCAN_EN_s,
+        DUT_RESET=>DUT_RESET_s,
+        LFSR_MISR_RESET=>LFSR_MISR_RESET_s
     );
     
+    rom_i: ROM 
+    port map(
+        addr=>SEED_ADDR_s,
+        dout=>ROM_OUT_s
+    );
+
     clkgen: process 
     begin
         clk_s<='0';
@@ -52,13 +75,9 @@ begin
         rst_s<='0';
         wait for 2*clkper;
         TEST_s<='1';
-        wait for clkper;
-        MISR_OUT_s<="0100100101010100101010010110010101001010101001010101010000100101";
-        wait for clkper;
-        MISR_OUT_s<="0100100101010100101010010110010101001010101001010101010000100101";
-        wait for clkper;
         MISR_OUT_s<="0101010100101010101001010101001010101010010101010010011010101010";
-        wait for clkper;
+        wait until END_TEST_s='1';
+        assert GO_s='1' report "ERROR NO GO";
         TEST_s<='0';
         wait for clkper;
         wait;
